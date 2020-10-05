1e2c5a15ac6d74dc828c704bf7849a1e6392be99
InterstellaR-007 <intrstelr.007@gmail.com>
2020-10-05 09:14:00 +0530

    [Anuj] Master Program

---
+++ regex_test/PinCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace regex_test
+{
+    class PinCode
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Pin Code Verification Program");
+            Console.WriteLine("Enter the pincode :");
+            String input_PinCode = Console.ReadLine();
+
+            String pattern = "[0-9]{6}";
+            
+            Regex regex = new Regex(pattern);
+
+            Match match = regex.Match(input_PinCode);
+            if (match.Success)
+            {
+                Console.WriteLine("Valid Pincode: " + match.Value);
+            }
+            else
+                Console.WriteLine("Invalid Pincode");
+
+        }
+    }
+}
